6fde69664222ce3f47db77a76097a7fa8b4596f6
Muhammad Zeeshan Khan <muhammadzeeshan123345@gmail.com>
2025-12-02 21:28:38 +0500

    Update sst_custom.py

--- src/utils/sst_custom.py
+++ src/utils/sst_custom.py
@@ -11,3 +11,5 @@ def singular_spectrum_transform(signal, window=30, lag=10, n_components=10):
     # Ensure feasible parameters and symmetric windows
     if window >= N or lag > window:
         return scores
+    if N - window <= window:
+        return scores
