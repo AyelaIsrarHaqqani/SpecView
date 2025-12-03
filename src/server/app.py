71fa8c8e302390675bfa87659c037c3b5229ed13
Muhammad Zeeshan Khan <muhammadzeeshan123345@gmail.com>
2025-12-03 20:12:30 +0500

    Update app.py

--- src/server/app.py
+++ src/server/app.py
@@ -6,3 +6,5 @@ import numpy as np
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
+
+from src.inference.run_inference import load_artifacts, load_signal, predict_signal
