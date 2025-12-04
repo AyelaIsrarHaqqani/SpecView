a10b08b4360d2cbb52447fc597ccc15052f6d7ab
Umar <umarr.k220@gmail.com>
2025-12-04 23:32:16 +0500

    Update vizualization.py

--- src/utils/vizualization.py
+++ src/utils/vizualization.py
@@ -1,3 +1,8 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
+
+def plot_sst_spectrum(signal, spectrum):
+    plt.figure(figsize=(10,4))
+    plt.plot(signal, label='Original')
+    plt.plot(spectrum, label='SST Spectrum', alpha=0.7)
