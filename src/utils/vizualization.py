3c54d77fba2123d49b81ead6f1e32f5594d6121b
Umar <umarr.k220@gmail.com>
2025-12-04 23:32:28 +0500

    Update vizualization.py

--- src/utils/vizualization.py
+++ src/utils/vizualization.py
@@ -6,3 +6,4 @@ def plot_sst_spectrum(signal, spectrum):
     plt.figure(figsize=(10,4))
     plt.plot(signal, label='Original')
     plt.plot(spectrum, label='SST Spectrum', alpha=0.7)
+    plt.legend(); plt.title('SST Spectrum Visualization'); plt.show()
