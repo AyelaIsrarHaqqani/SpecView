663505fd67a3ac738dffc06dedabc9ce7dbf1942
Umar <umarr.k220@gmail.com>
2025-12-07 17:00:25 +0500

    Update export_pso.py

--- src/feature_extraction/export_pso.py
+++ src/feature_extraction/export_pso.py
@@ -5,3 +5,8 @@ from src.utils.file_loader import load_dataset
 from src.utils.resampler import resample_signal
 from src.config import CONFIG
 from .pso_optimizer import optimize_sst_params_and_save
+
+def main():
+    parser = argparse.ArgumentParser(description="Run PSO to optimize SST params and save them to a file.")
+    parser.add_argument("--data", default="malimg_dataset/train", help="Path to dataset root (label subdirs).")
+    
