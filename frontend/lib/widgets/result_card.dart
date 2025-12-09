20f6ab5e26d2a9333d08a93f6b77213f8be52a71
Umar <umarr.k220@gmail.com>
2025-12-09 21:15:27 +0500

    Update result_card.dart

--- frontend/lib/widgets/result_card.dart
+++ frontend/lib/widgets/result_card.dart
@@ -1,1 +1,6 @@
 import 'package:flutter/material.dart';
+
+class ResultCard extends StatelessWidget {
+  final String label;
+  final double confidence;
+  const ResultCard({super.key, required this.label, required this.confidence});
