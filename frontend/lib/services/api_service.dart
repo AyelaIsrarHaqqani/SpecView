3b2f0dba3ace04e5d38f62cd476a8b21b79a16b7
Umar <umarr.k220@gmail.com>
2025-12-09 20:57:14 +0500

    Update api_service.dart

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -3,3 +3,11 @@ import 'dart:io' show File;
 
 import 'package:http/http.dart' as http;
 import 'package:file_picker/file_picker.dart';
+
+
+class InferenceResult {
+  final String label;
+  final double confidence;
+
+  InferenceResult({required this.label, required this.confidence});
+}
